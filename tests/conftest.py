bc101478c4f973c9de3b4f1f5b793e80b3b19d88
Jeremiah Anku Coblah <jeremiah.coblah@amalitech.com>
2026-02-27 10:16:30 +0000

    fix: etl_monitoring not exist error fixed

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,25 @@
 import pytest
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 import os
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def engine():
     engine = create_engine(
-        f"postgresql+psycopg2://"
-        f"{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}"
-        f"@{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT', '5432')}"
-        f"/{os.getenv('POSTGRES_DB')}"
+        f"postgresql+psycopg2://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT', 5432)}/{os.getenv('POSTGRES_DB')}"
     )
+    # Create tables if not exist
+    with engine.begin() as conn:
+        conn.execute(text("""
+            CREATE TABLE IF NOT EXISTS etl_monitoring (
+                run_id SERIAL PRIMARY KEY,
+                run_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                total_rows INT,
+                valid_rows INT,
+                invalid_rows INT,
+                invalid_ratio FLOAT,
+                load_duration_seconds FLOAT,
+                status VARCHAR(20)
+            )
+        """))
     yield engine
     engine.dispose()
